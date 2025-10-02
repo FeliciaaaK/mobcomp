40643592c6f502a5d2142a032a6c8c9f2b501409
FeliciaaaK <151896580+FeliciaaaK@users.noreply.github.com>
2025-10-02 14:06:59 +0700

    Add files via upload

---
+++ W4_MobComp/W4_MobComp/W4_MobCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  W4_MobCompApp.swift
+//  W4_MobComp
+//
+//  Created by student on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct W4_MobCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
